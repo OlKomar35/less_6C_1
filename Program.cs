3e92b7cc42bc0f9b6361b705272e7554c4a85b03
Olga Komar <komar_ol@mail.ru>
2023-05-02 13:28:38 +0300

    added the solution to the first task

--- Program.cs
+++ Program.cs
@@ -4,3 +4,29 @@
 //0, 7, 8, -2, -2 -> 2
 //1, -7, 567, 89, 223-> 3
 
+void PrintArray(int[]arr){
+    Console.Write("[");
+for (int i = 0; i < arr.Length; i++)
+{
+    if(i!=arr.Length-1){
+        Console.Write($"{arr[i]}, ");
+    }else{
+        Console.WriteLine($"{arr[i]}]");
+    }
+}
+}
+
+Console.Write("Введите количество цифр M=");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите числа через запятую: ");
+string s = Console.ReadLine();
+string [] stringArr = s.Split(new char[]{' ',',','.'},System.StringSplitOptions.RemoveEmptyEntries);
+
+int[] numbers = stringArr.Select(Int32.Parse).ToArray();
+PrintArray(numbers);
+int count =0;
+foreach (int num in numbers)
+{
+    if(num>0)count++;
+}
+Console.WriteLine($"Чисел больше 0 в введенном массиве ={count}");
